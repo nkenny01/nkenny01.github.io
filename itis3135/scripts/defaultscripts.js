5e9fc96bc24dc0ff80cfac2e38a66dac06b4d4e9
nkenny01 <72165718+nkenny01@users.noreply.github.com>
2022-02-17 12:11:08 -0500

    polygon and script changes

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -33,3 +33,62 @@ function headsOrTails() {
 function randomNumGenerator() {
     alert(parseInt(Math.random() * 101));
 }
+
+function getShape(index) {
+    switch (index) {
+        case 0:
+            alert("There is no polygon with 0 sides");
+            return true;
+        case 1:
+            alert("A polygon with 1 side is called a henagon or monogon");
+            return true;
+        case 2:
+            alert("A polygon with 2 sides is called a digon or bigon");
+            return true;
+        case 3:
+            alert("A polygon with 3 sides is called a trigon or trinagle");
+            return true; 
+        case 4:
+            alert("A polygon with 4 sides is called a tetragon or quadrilateral");
+            return true;
+        case 5:
+            alert("A polygon with 5 sides is called a pentagon");
+            return true;
+        case 6: 
+            alert("A polygon with 6 sides is called a hexagon");
+            return true;
+        case 7:
+            alert("A polygon with 7 sides is called a heptagon or septagon");
+            return true;
+        case 8:
+            alert("A polygon with 8 sides is called an octagon");
+            return true;
+        case 9:
+            alert("A polygon with 9 sides is called a nonagon");
+            return true;
+        case 10:
+            alert("A polygon with 10 sides is called a decagon");
+            return true;
+        default:
+            return false;
+        
+    }
+}
+
+function validateEntry(value) {
+    if (value >= -10 && value <= 10) {
+        if (Number.isInteger(value)) {
+            value = parseInt(value);
+            value = Math.abs(value);
+            return value;
+        }
+        else {
+            value = parseInt(parseFloat(value));
+            value = Math.abs(value);
+            return value;
+        }
+    }
+    else {
+        alert("This is invalid. Please enter an integer from 0-10:")
+    }
+}
