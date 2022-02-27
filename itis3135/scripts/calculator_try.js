50069b57e44d4de29367a89fc7be83281684f26b
nkenny01 <72165718+nkenny01@users.noreply.github.com>
2022-02-27 16:14:43 -0500

    changes

---
+++ itis3135/scripts/calculator_try.js
@@ -0,0 +1,35 @@
+function add() {
+    var num1 = prompt("Enter first number: ");
+    var num2 = prompt("Enter second number: ");
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    var result = num1 + num2;
+    alert(num1 + " + " + num2 + " = " + result)
+}
+
+function subtract() {
+    var num1 = prompt("Enter first number: ");
+    var num2 = prompt("Enter second number: ");
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    var result = num1 - num2;
+    alert(num1 + " - " + num2 + " = " + result)
+}
+
+function multiply() {
+    var num1 = prompt("Enter first number: ");
+    var num2 = prompt("Enter second number: ");
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    var result = num1 * num2;
+    alert(num1 + " * " + num2 + " = " + result)
+}
+
+function divide() {
+    var num1 = prompt("Enter first number: ");
+    var num2 = prompt("Enter second number: ");
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    var result = num1 / num2;
+    alert(num1 + " ÷ " + num2 + " = " + result)
+}
